9768c72d6d2ac83fffbc47426e7ac5bae2281793
PinkjeetSinghSandhu <62374048+PinkjeetSinghSandhu@users.noreply.github.com>
2020-04-07 15:57:55 +0930

    Add files via upload

---
+++ delete_calendar.php
@@ -0,0 +1,25 @@
+<?php
+include("includes/config.php");
+$data = array();
+
+$query = "SELECT * FROM events ORDER BY id";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+foreach($result as $row)
+{
+ $data[] = array(
+  'id'   => $row["id"],
+  'title'   => $row["title"],
+  'start'   => $row["start_event"],
+  'end'   => $row["end_event"]
+ );
+}
+
+echo json_encode($data);
+
+?>
